de0ae45a70e9f2e6cb2da2a7f2dee3ca0f7f8b04
jaywcjlove <398188662@qq.com>
2022-03-15 02:40:28 +0800

    init project.

---
+++ README.md
@@ -0,0 +1,32 @@
+dark-mode
+===
+
+A custom element that allows you to easily put a Dark Mode 🌒 toggle. so you can initially adhere to your users' preferences according to [`prefers-color-scheme`](https://drafts.csswg.org/mediaqueries-5/#prefers-color-scheme), but also allow them to (optionally permanently) override their system setting for just your site.
+
+## Installation
+
+Install from npm:
+
+```bash
+npm install --save dark-mode-toggle
+```
+
+Or, alternatively, use a `<script defer>` tag (served from unpkg's CDN):
+
+```html
+<script src="https://unpkg.com/@wcj/dark-mode" defer></script>
+```
+
+## Usage
+
+There are two ways how you can use `<dark-mode>`:
+
+
+```html
+<dark-mode></dark-mode>
+<dark-mode dark="Dark" light="Light" style="border: 1px solid red; font-size: 12px;"></dark-mode>
+```
+
+## License
+
+Licensed under the [MIT License](https://opensource.org/licenses/MIT).
---
+++ main.js
@@ -0,0 +1,123 @@
+const doc = document;
+const STYLE_ID = '_dark_mode_style_';
+const COLOR_SCHEME_CHANGE = 'colorschemechange';
+
+// See https://html.spec.whatwg.org/multipage/common-dom-interfaces.html ↵
+// #reflecting-content-attributes-in-idl-attributes.
+const installStringReflection = (obj, attrName, propName = attrName) => {
+  Object.defineProperty(obj, propName, {
+    enumerable: true,
+    get() {
+      const value = this.getAttribute(attrName);
+      return value === null ? '' : value;
+    },
+    set(v) {
+      this.setAttribute(attrName, v);
+    },
+  });
+};
+
+class DarkMode extends HTMLElement {
+  static get observedAttributes() {
+    return ['mode'];
+  }
+  constructor() {
+    super();
+    installStringReflection(this, 'mode');
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      this.mode = 'dark';
+      this._changeThemeTag();
+    }
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+      this.mode = 'light';
+      this._changeThemeTag();
+    }
+    window.matchMedia('(prefers-color-scheme: light)').onchange = (event) => {
+      this.mode = event.matches ? "light" : "dark";
+      this._changeThemeTag();
+    }
+    window.matchMedia('(prefers-color-scheme: dark)').onchange = (event) => {
+      this.mode = event.matches ? "dark" : "light";
+      this._changeThemeTag();
+    }
+
+    const observer = new MutationObserver((mutationsList, observer) => {
+      this.mode = document.body.dataset.colorMode;
+      this._changeContent();
+      this._dispatchEvent(COLOR_SCHEME_CHANGE, { colorScheme: this.mode });
+    });
+    // 以上述配置开始观察目标节点
+    observer.observe(doc.body, { attributes: true });
+    // 之后，可停止观察
+    // observer.disconnect();
+    this._initializeDOM();
+  }
+  _changeThemeTag() {
+    doc.body.setAttribute('data-color-mode', this.mode);
+  }
+  _changeContent() {
+    this.icon.textContent = this.mode === 'light' ? '🌒' : '🌞';
+    this.text.textContent = this.mode === 'light' ? this.getAttribute('dark') : this.getAttribute('light');
+  }
+  _initializeDOM() {
+    var shadow = this.attachShadow({ mode: 'open' });
+    var label = doc.createElement('span');
+    label.setAttribute('class', 'wrapper');
+    label.onclick = () => {
+      this.mode = this.mode === 'light' ? 'dark' : 'light';
+      this._changeThemeTag();
+      this._changeContent();
+    }
+    shadow.appendChild(label);
+
+    this.icon = doc.createElement('span');
+    label.appendChild(this.icon);
+
+    this.text = doc.createElement('span');
+    label.appendChild(this.text);
+    this._changeContent();
+
+    const textContent = `
+[data-color-mode*='dark'] {
+  color-scheme: dark;
+  --color-thme-bg: #0d1117;
+  --color-thme-text: #fff;
+  background-color: var(--color-thme-bg);
+  color: var(--color-thme-text);
+}
+
+[data-color-mode*='light'] {
+  color-scheme: light;
+  --color-thme-bg: #fff;
+  --color-thme-text: #282c34;
+  background-color: var(--color-thme-bg);
+  color: var(--color-thme-text);
+}`;
+
+    const styleDom = doc.getElementById(STYLE_ID);
+    
+    if (!styleDom) {
+      var initstyle = doc.createElement('style');
+      initstyle.id = STYLE_ID;
+      initstyle.textContent = textContent;
+      doc.head.appendChild(initstyle);
+    }
+
+    var customStyle = this.getAttribute('style');
+    var style = doc.createElement('style');
+    style.textContent = `
+    .wrapper { cursor: pointer; user-select: none; position: relative; ${customStyle ? customStyle : ''} }
+    .wrapper > span + span { margin-left: .4rem; }
+    `;
+    shadow.appendChild(style);
+  }
+  _dispatchEvent(type, value) {
+    this.dispatchEvent(new CustomEvent(type, {
+      bubbles: true,
+      composed: true,
+      detail: value,
+    }));
+  }
+}
+
+customElements.define('dark-mode', DarkMode);
