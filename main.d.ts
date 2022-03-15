8f5585bfe4212f76fc1e094200b2b4339d07cfd9
jaywcjlove <398188662@qq.com>
2022-03-15 16:02:01 +0800

    type: Add type file.

---
+++ main.d.ts
@@ -0,0 +1,24 @@
+
+export type ColorScheme = 'light' | 'dark';
+export type ColorSchemeChangeEvent = CustomEvent<{ colorScheme: ColorScheme }>;
+export class DarkMode extends HTMLElement {
+  mode?: ColorScheme;
+  dark?: string;
+  light?: string;
+  style?: React.CSSProperties;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'dark-mode': DarkMode;
+  }
+  interface GlobalEventHandlersEventMap {
+    'colorschemechange': ColorSchemeChangeEvent;
+  }
+}
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    'dark-mode': DarkMode;
+  }
+}
